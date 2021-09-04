34cd7914297e400214762c5d974311b14a90e906
UTR-ADP <utr160626@gmail.com>
2021-09-04 17:58:45 +0900

    IntArray

---
+++ algorithm_java/src/chap2/IntArray.java
@@ -0,0 +1,15 @@
+package chap2;
+
+public class IntArray {
+	public static void main (String[] args) {
+		int[] a = new int[5]; //¿Œµ¶Ω∫ 0 ~ 4
+		
+		a[1] = 452;
+		a[2] = 42;
+		a[4] = a[1] * 2;
+		
+		for (int i = 0; i < a.length; i++) {
+			System.out.println("a[" + i + "] = " + a[i] );
+		}
+	}
+}
