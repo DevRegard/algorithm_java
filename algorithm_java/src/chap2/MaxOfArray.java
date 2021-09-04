d0dbfc8ebe91db7b7de5351cfc5364d5388389ed
UTR-ADP <utr160626@gmail.com>
2021-09-04 17:59:57 +0900

    사람수 입력 메서드와 배열의 최댓값 메서드

---
+++ algorithm_java/src/chap2/MaxOfArray.java
@@ -0,0 +1,43 @@
+package chap2;
+
+import java.util.Scanner;
+
+public class MaxOfArray {
+	
+	private static Scanner scan;
+	
+	//배열의 최댓값 메서드
+	public static int maxOf(int[] a) {
+	
+		int max = a[0];
+		
+		for (int i = 0; i < a.length; i++) {
+			if(a[i] > max) {
+				max = a[i];
+			}
+		}
+		
+		return max;	
+	}
+	
+	//사람수 입력 메서드
+	public static int[] height() {
+		scan = new Scanner(System.in);
+		System.out.println("가장 키가 큰 사람");
+		System.out.print("사람 수 :  "); 
+		int num = scan.nextInt();
+		
+		int[] height = new int[num];
+		
+		for (int i = 0; i < num; i++) {
+			System.out.println("height[" + i + "] : ");
+			height[i] = scan.nextInt();
+		}
+		
+		return height;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("가장 큰 키는 " + maxOf(height()) + "입니다.");
+	}
+}
