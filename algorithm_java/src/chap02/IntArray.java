c3ecba814237b1286311ac3b6255d4cf5aa79de1
UTR-ADP <utr160626@gmail.com>
2021-09-04 18:06:07 +0900

    change Package Name "chap2" to "chap02"

--- algorithm_java/src/chap02/IntArray.java
+++ algorithm_java/src/chap02/IntArray.java
@@ -1,4 +1,4 @@
-package chap2;
+package chap02;
 
 public class IntArray {
 	public static void main (String[] args) {
--- algorithm_java/src/chap02/IntArrayInit.java
+++ algorithm_java/src/chap02/IntArrayInit.java
@@ -1,4 +1,4 @@
-package chap2;
+package chap02;
 
 public class IntArrayInit {
 
